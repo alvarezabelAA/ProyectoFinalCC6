1ac86379747136a1b5af5f33818742787ca63e45
Abel <abel.alvarez@galileo.edu>
2022-11-15 03:03:46 -0600

    Avanzes varios

---
+++ main/add_edit/tarea.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/add.css">
+    <script src="https://kit.fontawesome.com/1bb1b9f305.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container_form">
+        <h2>Registrar Tarea</h2>
+        <form action="">
+            <input type="text" placeholder="Tipo de Tarea">
+            <?php
+                include '../../data_base/conexion_db.php';
+                $nombre = '';
+
+                $query = "select * from usuarios order by nombre ";
+
+            ?>
+            <select name = "usuario">
+                <?php
+                    $execute = mysqli_query($conexion, $query);
+                    while ($line = mysqli_fetch_array($execute, MYSQLI_ASSOC)) {
+                        $nombre=$line["nombre"];
+                        $id_usuario=$line["id_usuario"];
+                        echo "<option value=$id_usuario>$nombre</option>\n";
+                     }
+                ?>
+            </select>
+            <input type="text" placeholder="Contenido a realizar">
+            <button>Agregar Tarea</button>
+        </form>
+    </div>
+</body>
+</html>
