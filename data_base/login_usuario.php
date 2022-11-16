e358bc4816e0ab9148d9784e8c304a29987fde09
jairoDGM <jairo.danielgm@gmail.com>
2022-11-15 23:09:47 -0600

    modificacion campo nuevo de SESSION

--- data_base/login_usuario.php
+++ data_base/login_usuario.php
@@ -6,11 +6,15 @@
     $usuario = $_POST['usuario'];
     $contrasena = $_POST['contrasena'];
 
-    $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario='$usuario'
+    $validar_login = mysqli_query($conexion, "SELECT id_usuario FROM usuarios WHERE usuario='$usuario'
                             and contrasena = '$contrasena'");
 
     if (mysqli_num_rows($validar_login) > 0) {
         $_SESSION['usuario']=$usuario;
+        while ($line = mysqli_fetch_array($validar_login, MYSQLI_ASSOC)) {
+            $_SESSION["id_usuario2"]=$line["id_usuario"];
+         }
+     
         header("location: ../main/principal.php");
     } else {
         echo '
